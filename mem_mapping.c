f920ac2e4f77b6006bb2bd918e4b175b1fda3ba4
Sabrina Lesser <brinalesser@vt.edu>
2021-12-02 14:49:55 -0500

    memory mapping

--- mem_mapping.c
+++ mem_mapping.c
@@ -13,6 +13,16 @@ void main(){
 	fgets(mem_loc_str, sizeof mem_loc_str, stdin);
 	
 	char mem_size_str[100];
-	printf("Enter a memory location: ");
-	fgets(mem_loc_str, sizeof mem_loc_str, stdin);
+	printf("Enter number of integers to read: ");
+	fgets(mem_size_str, sizeof mem_size_str, stdin);
+	
+	int * p = (int *)strtol(mem_loc_str, NULL, 16);
+	int mem_size = atoi(mem_size_str);
+	
+	int i;
+	printf("The value at that memory location is [");
+	for(i = 0; i < mem_size - 1; i++){
+		printf("%d, ", (*(p+1)));
+	}
+	printf("%d ]\n", (*(p+1)));
 }
