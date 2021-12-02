504ee33d0d4a4b806c13976fd5d0589c2393a9db
brinalesser <49353127+brinalesser@users.noreply.github.com>
2021-12-02 14:45:19 -0500

    Add files via upload

---
+++ mem_mapping.c
@@ -0,0 +1,18 @@
+int gui_value1;
+int gui_value2;
+int gui_value3;
+int gui_value_array[5] = {1, 2, 3, 4, 5};
+
+void main(){
+	gui_value1 = 1;
+	gui_value2 = 2;
+	gui_value3 = 3;
+	
+	char mem_loc_str[100];
+	printf("Enter a memory location: ");
+	fgets(mem_loc_str, sizeof mem_loc_str, stdin);
+	
+	char mem_size_str[100];
+	printf("Enter a memory location: ");
+	fgets(mem_loc_str, sizeof mem_loc_str, stdin);
+}
