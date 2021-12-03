151be0b37fe6a45b7ffcd121130a17c150055666
brinalesser <49353127+brinalesser@users.noreply.github.com>
2021-12-03 16:52:42 -0500

    Update mqtt_pub.c

--- mqtt_pub.c
+++ mqtt_pub.c
@@ -7,12 +7,10 @@ int gui_value1 = 1;
 int gui_value2 = 2;
 int gui_value3 = 3;
 
-int get_mem_at_location(char * loc){	
-	/**
-	int * p = (int *)strtol(loc, NULL, 16);
-	int val =  *p;
-	**/
-	return 42;
+int get_mem_at_location(char * loc){
+        int * p = (int *)strtol(loc, NULL, 16);
+        int val =  *p;
+        return val;
 }
 
 int main(){
@@ -23,28 +21,27 @@ int main(){
 
 	mosq = mosquitto_new("publisher-test", true, NULL);
 
-	rc = mosquitto_connect(mosq, "localhost", 15672, 60);
+	rc = mosquitto_connect(mosq, "localhost", 5672, 60);
 	if(rc != 0){
 		printf("Client could not connect to broker! Error Code: %d\n", rc);
 		mosquitto_destroy(mosq);
 		return -1;
 	}
 	printf("We are now connected to the broker!\n");
-	
-	char mem_loc_str[100];
-	printf("Enter a memory location: ");
-	fgets(mem_loc_str, sizeof mem_loc_str, stdin);
-	
-	int val_to_send = get_mem_at_location(mem_loc_str);
 
-	char char_val[6];
-	sprintf(char_val, "%d", val_to_send);
-	
-	mosquitto_publish(mosq, NULL, "test/t1", 6, char_val, 0, false);
+        char mem_loc_str[100];
+        printf("Enter a memory location: ");
+        fgets(mem_loc_str, sizeof mem_loc_str, stdin);
 
-	mosquitto_disconnect(mosq);
-	mosquitto_destroy(mosq);
+        int val_to_send = get_mem_at_location(mem_loc_str);
 
-	mosquitto_lib_cleanup();
-	return 0;
+        char char_val[6];
+        sprintf(char_val, "%d", val_to_send);
+        mosquitto_publish(mosq, NULL, "test/t1", 6, char_val, 0, false);
+
+        mosquitto_disconnect(mosq);
+        mosquitto_destroy(mosq);
+
+        mosquitto_lib_cleanup();
+        return 0;
 }
